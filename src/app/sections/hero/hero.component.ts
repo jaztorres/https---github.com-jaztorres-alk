1f0cb0718c9e8dbe1c13141760901d57a82201e5
jaztorres <jazmin.torres@myma.mx>
2025-05-02 09:01:05 -0600

    hero

--- src/app/sections/hero/hero.component.ts
+++ src/app/sections/hero/hero.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-hero',
-  //standalone: true,
-  //imports: [],
+  standalone: true,
+  imports: [],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss'
 })
