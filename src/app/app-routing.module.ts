352ed0920f6507a198b97d9e8112ab8ebbb54f98
jaztorres <jazmin.torres@myma.mx>
2025-05-02 22:09:29 -0600

    restructura

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
 import { HeaderComponent } from './layout/header/header.component';
+import { HeroComponent } from './sections/hero/hero.component';
+import { MecanicaComponent } from './sections/mecanica/mecanica.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { MemoramaComponent } from './sections/memorama/memorama.component';
+import { ParticipacionComponent } from './sections/participacion/participacion.component';
+import { GanadoresComponent } from './sections/ganadores/ganadores.component';
+import { PremiosComponent } from './sections/premios/premios.component';
+
 
 const routes: Routes = [
   { path: 'header', component: HeaderComponent },
+  { path: 'footer', component: FooterComponent },
+  { path: 'hero', component: HeroComponent },
+  { path: 'mecanica', component: MecanicaComponent },
+  { path: 'memorama', component: MemoramaComponent },
+  { path: 'participacion', component: ParticipacionComponent },
+  { path: 'ganadores', component: GanadoresComponent },
+  { path: 'premios', component:PremiosComponent},
   
   //seccionHome
-  { path: '**', pathMatch: 'full', redirectTo: 'home' },
-  { path: '', pathMatch: 'full', redirectTo:'home' }
+  { path: '**', pathMatch: 'full', redirectTo: 'hero' },
+  { path: '', pathMatch: 'full', redirectTo:'hero' }
 ];
 
 
-
-
-
 @NgModule({
-  imports: [RouterModule.forRoot(routes,{
+  imports: [ RouterModule.forRoot(routes,{
     onSameUrlNavigation: "ignore",
     anchorScrolling:'enabled',
     scrollPositionRestoration: 'enabled'
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeroComponent } from './sections/hero/hero.component';
-import { HeaderComponent } from './layout/header/header.component';
+
 
 @Component({
   selector: 'app-root',
   //standalone: true,
-  //imports: [RouterOutlet, HeroComponent, HeaderComponent],
+//imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'alk-def';
